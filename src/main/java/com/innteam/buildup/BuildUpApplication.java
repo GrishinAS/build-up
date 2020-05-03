f1bdb40f4925255234c0307a3bb3c1a6c274406f
GrishinAS <anton.grishin@mts.ru>
2020-05-03 13:45:13 +0300

    initial commit

---
+++ src/main/java/com/innteam/buildup/BuildUpApplication.java
@@ -0,0 +1,13 @@
+package com.innteam.buildup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildUpApplication.class, args);
+	}
+
+}
