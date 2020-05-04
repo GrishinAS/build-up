e599719da53ecec97ee4d6c0a902ba6e51e297d0
alxndrtarasov <a.tarasov@diginavis.com>
2020-05-04 14:28:57 +0300

    working on roadMap

--- src/main/java/com/innteam/buildup/commons/model/roadFolders/PointCrudService.java
+++ src/main/java/com/innteam/buildup/commons/model/roadFolders/PointCrudService.java
@@ -3,14 +3,21 @@ package com.innteam.buildup.commons.model.roadFolders;
 import com.innteam.buildup.commons.model.DomainObjectCrudServiceBase;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Service
+@Scope("singleton")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PointCrudService extends DomainObjectCrudServiceBase<RoadPoint> {
+    @Autowired
+    RoadFolderCrudService folderCrudService;
 
     public PointCrudService(PagingAndSortingRepository<RoadPoint, UUID> repo) {
         super(repo);
@@ -18,6 +25,9 @@ public class PointCrudService extends DomainObjectCrudServiceBase<RoadPoint> {
 
     public List<RoadPoint> getMock() {
         List<RoadPoint> result = new ArrayList<>();
-        return result;
+
+
+        final RoadFolder folder = folderCrudService.read(folderCrudService.create(new RoadFolder("Java", result)));
+        return folder.getInternal();
     }
 }
